dbfa178672f0b81eb8d36c3ea9b77deb2be1471a
Vince Jones <vincethebutcher@gmail.com>
2016-01-02 14:45:54 -0800

    Uses Factory

--- index.php
+++ index.php
@@ -5,28 +5,11 @@
  * @author Vince Jones <vjones@nerdery.com>
  */
 
-
-use PHPWeekly\Entity\Prisoner;
-use PHPWeekly\Service\Deliberator;
-use PHPWeekly\Service\FileWhisperer;
-
+use PHPWeekly\Factory\PrisonerFactory;
 
 require_once 'vendor/autoload.php';
 
-$otherGuy= new Prisoner($argv[1], $argv[2], $argv[3]);
-$me = new Prisoner("Vince", "php", $argv[4]);
-
-
-$otherGuyFile = new FileWhisperer($otherGuy);
-$myFile = new FileWhisperer($me);
-
-$deliberator = new Deliberator($me, $otherGuy);
-$answer = $deliberator->getAnswer($myFile, $otherGuyFile);
-
-$otherGuyFile->writeFile($otherGuy);
-$myFile->writeFile($me);
-
-$scores = $deliberator->getScore($myFile, $otherGuyFile);
+$prisonerFactory = new PrisonerFactory($argv);
+$answer = $prisonerFactory->getAnswer();
 
 echo $answer.PHP_EOL;
-
--- src/Factory/PrisonerFactory.php
+++ src/Factory/PrisonerFactory.php
@@ -3,6 +3,8 @@
 namespace PHPWeekly\Factory;
 
 use PHPWeekly\Entity\Prisoner;
+use PHPWeekly\Service\Deliberator;
+use PHPWeekly\Service\FileWhisperer;
 
 /**
  * Class PrisonerFactory
@@ -12,20 +14,32 @@ class PrisonerFactory
 	{
 
 	/**
-	 * @param string $name
-	 * @param string $discipline
-	 * @param string $previousResponse
+	 * @param $arguments
      */
-	public function __construct( $name = 'Buck', $discipline = 'php', $previousResponse = "silent" )
+	public function __construct( $arguments )
 	{
+		$this->otherGuy = new Prisoner($arguments[1], $arguments[2], $arguments[3]);
+		$this->me = new Prisoner("Vince", "php", $arguments[4]);
 
-		if ( $previousResponse != 'confess' || $previousResponse != 'silent' ) {
-			$previousResponse = 'silent';
-		}
+		$this->otherGuyFile = new FileWhisperer($this->otherGuy);
+		$this->myFile = new FileWhisperer($this->me);
 
-		return new Prisoner($name, $discipline, $previousResponse);
+		$this->deliberator = new Deliberator($this->me, $this->otherGuy);
 	}
 
-	
 
+	/**
+	 * @return string
+     */
+	public function getAnswer()
+	{
+		$answer = $this->deliberator->getAnswer($this->myFile, $this->otherGuyFile);
+
+		$this->otherGuyFile->writeFile($this->otherGuy);
+		$this->myFile->writeFile($this->me);
+
+		$scores = $this->deliberator->getScore($this->myFile, $this->otherGuyFile);
+
+		return $answer;
+	}
 }
